203b8642f3e8a3d6ab0ee81590c987063d414303
Serge Iangicher <sergey.yangicher@rockyroad.io>
2025-08-10 17:57:53 +0300

    search box

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import ContactForm from './components/ContactForm/ContactForm';
+import SearchBox from './components/SearchBox/SearchBox';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   const onAddContact = data => {
   };
+  const onChangeSearchValue = evt => {
+    setSearchValue(evt.target.value);
+  };
+  const [searchValue, setSearchValue] = useState('');
 
   return (
     <>
       <h1>Phonebook</h1>
       <ContactForm handleAdd={onAddContact} />
+      <SearchBox value={searchValue} handleChange={onChangeSearchValue} />
     </>
   );
 }
